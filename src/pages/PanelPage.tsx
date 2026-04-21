817c47598d4e16e7dd419afee05259fb819b3ccc
Romina Ruiz <ruizdiazsanchezromina@gmail.com>
2026-04-21 20:33:37 +0200

    IncidentManager-14: Listado de las incidencias en el panelPage

--- src/pages/PanelPage.tsx
+++ src/pages/PanelPage.tsx
@@ -1,26 +1,9 @@
-import { useIncidencias } from '@/hooks/useIncidencias'
-import { useNavigate } from 'react-router-dom'
-import { IncidenciaCard } from '@/components/features/incidencias/IncidenciaCard'
+import { IncidenciasSection } from '@/components/features/incidencias/IncidenciasSection'
 
 export const PanelPage = () => {
-  const { incidencias, loading } = useIncidencias()
-  const navigate = useNavigate()
-
-  if (loading) return <p className="p-6">Cargando...</p>
-
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Incidencias</h1>
-
-      <div className="grid gap-4">
-        {incidencias.map(inc => (
-          <IncidenciaCard
-            key={inc.id}
-            incidencia={inc}
-            onClick={() => navigate(`/incidencia/${inc.id}`)}
-          />
-        ))}
-      </div>
+      <IncidenciasSection />
     </div>
   )
 }
